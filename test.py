be3db41adb4240afd34810ea7188364c7e1996ed
Martzyy <Martzyy@users.noreply.github.com>
2020-04-17 15:21:36 +0800

    charlotte

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import pickle
 import preprocessing
 
-preprocessing.generate_data()
-print("done.")
+#preprocessing.generate_data()
+#print("done.")
 
 with open(b'network.p','rb') as f:
     data = pickle.load(f)
 
 for author in data.authors:
     if author.instituted is not None:
-        print(author.instituted)
+        print(author.instituted.name)
